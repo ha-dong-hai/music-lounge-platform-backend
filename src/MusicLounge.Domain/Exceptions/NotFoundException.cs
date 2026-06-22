e72f5da72ac299a5b23f93ce91a351cbf6456fb2
VitVuong <hadonghai17122001@gmail.com>
2026-06-22 17:06:15 +0700

    MLACP_0001 [REM] MusicLounge.Api : Add BaseEntity and NotFoundException

---
+++ src/MusicLounge.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+namespace MusicLounge.Domain.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string name, object key)
+        : base($"'{name}' with key ({key}) was not found.") { }
+}
