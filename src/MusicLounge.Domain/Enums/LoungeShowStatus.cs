9ed3484a389105e01aaee2529ff33b3acdd0c79a
VitVuong <hadonghai17122001@gmail.com>
2026-06-22 17:29:47 +0700

    MLACP_0001 [ADD] MusicLounge.Domain\Enums : Add LoungeShowStatus

---
+++ src/MusicLounge.Domain/Enums/LoungeShowStatus.cs
@@ -0,0 +1,11 @@
+namespace MusicLounge.Domain.Enums;
+
+public enum LoungeShowStatus
+{
+    Draft = 1,
+    Published = 2,
+    OnSale = 3,
+    SoldOut = 4,
+    Cancelled = 5,
+    Completed = 6
+}
