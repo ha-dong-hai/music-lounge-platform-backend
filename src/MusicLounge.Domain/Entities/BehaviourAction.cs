424cf251560dbb059e24ff6a5d2ebf84077f7bae
VitVuong <hadonghai17122001@gmail.com>
2026-06-24 23:32:33 +0700

    MLACP_0001 [ADD] (AI) MusicLounge.Domain\Enties : Add BehaviourAction enum and UserBehaviourLog entity to Domain Layer

---
+++ src/MusicLounge.Domain/Entities/BehaviourAction.cs
@@ -0,0 +1,14 @@
+namespace MusicLounge.Domain.Enums;
+
+public enum BehaviourAction
+{
+    ViewEvent = 1,
+    ViewEventLong = 2,
+    ViewLineup = 3,
+    ViewVenue = 4,
+    SearchGenre = 5,
+    WatchLivestream = 6,
+    ShareEvent = 7,
+    ClickTicket = 8,
+    ViewAfterWishlist = 9
+}
