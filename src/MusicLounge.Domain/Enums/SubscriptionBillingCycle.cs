2438a636777652fb36fc7675ec2fcd43bd4e1266
VitVuong <hadonghai17122001@gmail.com>
2026-06-25 19:51:07 +0700

    MLACP_0001 [ADD] (Venue) MusicLounge.Domain\Enums : Add venue and subscription enums

---
+++ src/MusicLounge.Domain/Enums/SubscriptionBillingCycle.cs
@@ -0,0 +1,10 @@
+// CoreFlow: CF6 (Payment & Revenue)
+// Billing frequency for Owner subscription packages.
+namespace MusicLounge.Domain.Enums;
+
+public enum SubscriptionBillingCycle
+{
+    Monthly = 1,
+    Quarterly = 2,
+    Yearly = 3
+}
